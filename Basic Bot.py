f9ed7205542df0cb495bf4da1b5fe498b2425abc
Habchy <habchy77@gmail.com>
2017-09-03 01:44:52 -0400

    Add files via upload
    This is the official Basic Bot, it shows you a basic representation of what you can do with python on Discord.

---
+++ Basic Bot.py
@@ -0,0 +1,46 @@
+# These are the dependecies. The bot depends on these to function, hence the name.
+import discord
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+from urllib.parse import urlencode
+import datetime
+import time
+import random
+import os
+from ast import literal_eval
+import aiohttp
+import json
+
+# Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
+client = Bot (description="Basic Bot by Habchy#1665", command_prefix="PUT YOUR PREFIX HERE", pm_help = True)
+
+# This is what happens everytime the bot launches. In this case, it prints information like server count and bot id in the console.
+@client.event
+async def on_ready():
+	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers')
+	print('--------')
+	print('Support Discord Server: https://discord.gg/FNNNgqb')
+	print('--------')
+	print('Created by Habchy#1665')
+	
+
+# This is a basic example of a call and response command. You tell it do "this" and it does it.
+@client.command()
+async def ping(*args):
+
+	await client.say(":ping_pong: Pong!")
+	await asyncio.sleep(3)
+	await client.say(":warning: This bot was created by **Habchy#1665**, it seems that you have not modified it yet. Go edit the file and try it out!")
+
+	
+client.run('PUT YOUR BOT TOKEN HERE')
+
+# Basic Bot was created by Habchy#1665
+# Please join this Discord server if you need help: https://discord.gg/FNNNgqb
+# Please modify the parts of the code where it asks you to. Example: The Prefix or The Bot Token
+# This is by no means a full bot, it's more of a starter to show you what the python language can do in Discord.
+# Thank you for using this and don't forget to star my repo on GitHub!
+
+# The help command is currently set to be Direct Messaged.
+# If you would like to change that, change "pm_help = True" to "pm_help = False" on line 14.
